47810259c0d6f18e6f40f0f14f6993ed210f317d
maria <mariaquintairosfernandez03@liceolapaz.net>
2025-02-06 12:22:49 +0100

    crear la pagina de Producto

---
+++ AppWebEventos/src/components/Productos/Producto.tsx
@@ -0,0 +1,40 @@
+import './Producto.css';
+
+import * as React from 'react';
+import { useState } from 'react';
+
+function Producto() {
+    
+   /*  const [productos, setProductos] = useState([
+        { id: 1, nombre: 'Producto 1', precio: 20.99 },
+        { id: 2, nombre: 'Producto 2', precio: 15.49 },
+        { id: 3, nombre: 'Producto 3', precio: 10.99 },
+        { id: 4, nombre: 'Producto 4', precio: 5.49 },
+        { id: 5, nombre: 'Producto 5', precio: 25.99 }
+    ]); */
+
+    return (
+       
+        <div className="productos">
+            <div className='busqueda'>
+                <input 
+                    type='text' 
+                    placeholder='Buscar producto' 
+                />
+                <select>
+                    <option value=''>Todas las categorías</option>
+                    <option value='categoria1'>Categoría 1</option>
+                    <option value='categoria2'>Categoría 2</option>
+                </select>
+                <input 
+                    type='number' 
+                    placeholder='Precio máximo' 
+                    value=""
+                />
+                <button>Buscar</button>
+            </div>
+        
+    );
+}
+
+export default Producto;
