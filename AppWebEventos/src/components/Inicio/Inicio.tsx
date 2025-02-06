2f376e3295923252aae89781ffbfdf64bab0948e
maria <mariaquintairosfernandez03@liceolapaz.net>
2025-02-06 12:47:00 +0100

    modificar el archivp de Inicio, añadiendo descripciones y otro contenido a la pagina

--- AppWebEventos/src/components/Inicio/Inicio.tsx
+++ AppWebEventos/src/components/Inicio/Inicio.tsx
@@ -8,6 +8,7 @@ import './Inicio.css';
 //import GestionEventosPagina from '../';
 
     const Inicio = () => {
+
         const productosDestacados = [
             { id: 1, nombre: 'Producto 1', precio: 20.99 },
             { id: 2, nombre: 'Producto 2', precio: 15.49 },
@@ -18,19 +19,27 @@ import './Inicio.css';
 
         return(
             <div className="Inicio">
-                {/* Contenido */
-                    //<GestionEventosPagina />
-                }
-                
-                
+                <div className='descripcion-tienda'>
                     <h1>Bienvenido a AppWebEventos</h1>
-                <h2>Sitio de gestión de eventos</h2> 
-                      
-                <p>Aquí podrás ver los eventos que se han creado y gestionarlos.</p>
-                <p>Si quieres ver los productos destacados, haz click en el botón "Productos destacados".</p>
-                <p>Si quieres ver los productos más vendidos, haz click en el botón "Productos más vendidos".</p>
-                <p>Si quieres ver los productos más buscados, haz click en el botón "Productos más buscados".</p> 
+                        <h2>Sitio de gestión de eventos</h2> 
+                        
+                        <p>Aquí podrás ver los eventos que se han creado y gestionarlos.</p>
+                        
+                        <p>
+                            Además aquí encontrarás todo lo que necesitas para organizar tus eventos especiales.
+                            ¡Descubre nuestra selección de productos y haz que tus celebraciones sean inolvidables!
+                        </p>
+                </div>
 
+                    <div className="productos-destacados">
+                        <h2>Productos destacados</h2>
+
+                        <p>Si quieres ver los productos destacados, haz click en el botón "Productos destacados".</p>
+                        <p>Si quieres ver los productos más vendidos, haz click en el botón "Productos más vendidos".</p>
+                        <p>Si quieres ver los productos más buscados, haz click en el botón "Productos más buscados".</p> 
+
+                        <a href={"../DetalleProducto/DetalleProducto.tsx"}>Ver detalles</a>
+                    </div>
             </div>
         )
     };
