2f2b3d2275e7936185ac6b606728d1998083847c
maria <mariaquintairosfernandez03@liceolapaz.net>
2025-02-21 20:34:06 +0100

    cambiar los enlaces a rutas

--- AppWebEventos/src/App.tsx
+++ AppWebEventos/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
 import './App.css';
 
-import DetalleProducto from './components/DetalleProducto/DetalleProducto';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import Inicio from './components/Inicio/Inicio';
-import Login from './components/Login/Login';
-import Productos from './components/Productos';
-import Registro from './components/Registro/Registro';
+
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
+    
+    <Router>
       <div className="App">
-        <Header />
-        <Inicio />
-        <Productos />
-
-        <DetalleProducto />
-
-        <Login />
-
-        <Registro />
-        
-        <Footer />
+        <Switch>
+          <Route path="/" exact component={Inicio} />
+          <Route path="/detalles/:id" component={DetallesProducto} />
+          <Route path="/iniciar-sesion" component={IniciarSesion} />
+        </Switch>
       </div>
-    </>
+    </Router>
+  
   );
 }
 
