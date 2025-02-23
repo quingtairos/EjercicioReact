9d70d9428bb6bfaee2b99e4fe726260defb3cf38
maria <mariaquintairosfernandez03@liceolapaz.net>
2025-02-24 00:21:42 +0100

    HACER LO mismo con el footer

--- AppWebEventos/src/App.tsx
+++ AppWebEventos/src/App.tsx
@@ -8,6 +8,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import React from 'react';
 import Carrito from './components/Carrito';
 import DetalleProducto from './components/DetalleProducto/DetalleProducto';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import IniciarSesion from './components/Login/Login';
 import Registro from './components/Registro';
@@ -16,20 +17,27 @@ const App: React.FC = () => {
   //const [count, setCount] = useState(0)
 
   return (
-    
-    <Router>
-      <Header />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={< Inicio />} />
-          <Route path="/producto/:id" element={<DetalleProducto /* match={undefined} */ />} />
-          <Route path="/carrito" Component={Carrito} />
-          <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-          <Route path="/registrarse" element={<Registro />} />
-        </Routes>
-      </div>
-    </Router>
   
+      <div className="App">
+        <header>
+          <h1>Bienvenido a mi página</h1>
+          <Header />
+        </header>
+        <main>
+          <p>Contenido principal de la web.</p>
+          <Router>
+          <Routes>
+            <Route path="/" element={< Inicio />} />
+            <Route path="/producto/:id" element={<DetalleProducto /* match={undefined} */ />} />
+            <Route path="/carrito" Component={Carrito} />
+            <Route path="/iniciar-sesion" element={<IniciarSesion />} />
+            <Route path="/registrarse" element={<Registro />} />
+          </Routes>
+          </Router>
+        </main>
+        <Footer />
+      </div>
+        
   );
 }
 
