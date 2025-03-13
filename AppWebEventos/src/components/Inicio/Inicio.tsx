a71c85af554097dd3a448d48e96617710ed8941d
maria <mariaquintairosfernandez03@liceolapaz.net>
2025-03-13 12:26:59 +0100

    añadir un mapa en la pagina de inicio

--- AppWebEventos/src/components/Inicio/Inicio.tsx
+++ AppWebEventos/src/components/Inicio/Inicio.tsx
@@ -116,7 +116,15 @@ import { useEffect } from 'react';
                                             <p>Si quieres ver los productos más buscados, haz click en el botón "Productos más buscados".</p> 
 
                                         </div>
+
+                                        <div className="store-map">
+                                          <h3>Visítanos en:</h3>
+                                          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d11607.448109511246!2d-8.364509641897033!3d43.33807614451293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1ses!2ses!4v1741864437023!5m2!1ses!2ses" 
+                                          width="600" height="450" style={{ border: 0 }} loading="lazy"> </iframe>
+                                        </div>
+
                                 </div>
+                                
         )
     };
 
